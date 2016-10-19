7727d3a97ab1be5a09d79078668b98bb60fbae15
Kensuke Nagae <kyanny@gmail.com>
2016-10-20 04:13:08 +0900

    Implement very basic parse/dump

--- mrblib/j.rb
+++ mrblib/j.rb
@@ -2,6 +2,9 @@ def __main__(argv)
   if argv[1] == "version"
     puts "v#{J::VERSION}"
   else
-    puts "Hello World"
+    puts JSON.generate(JSON.parse(STDIN.read), {
+                         pretty_print: true,
+                         indent_with: 2
+                       })
   end
 end
